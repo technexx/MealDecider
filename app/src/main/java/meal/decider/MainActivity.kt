2f0a162a52496afad1d984be3a68f8a79164f5a5
Brian Salomone <brian.j.salomone@gmail.com>
2023-12-19 12:32:18 -0800

    textfield working

--- app/src/main/java/meal/decider/MainActivity.kt
+++ app/src/main/java/meal/decider/MainActivity.kt
@@ -58,8 +58,11 @@ import android.location.Location
 import android.net.Uri
 import android.widget.Toast
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.MoreVert
@@ -71,8 +74,11 @@ import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
@@ -81,6 +87,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.window.Dialog
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
@@ -197,25 +206,6 @@ fun Board() {
         Spacer(modifier = Modifier.height(16.dp))
         InteractionLayout()
     }
-}
-
-@Composable
-fun EditDialog() {
-    AlertDialog(
-        onDismissRequest = {  gameViewModel.updateActiveEdit(false) },
-        title = { Text("Are you sure you want to delete this?") },
-        text = { Text("This action cannot be undone") },
-        confirmButton = {
-            TextButton(onClick = { /* TODO */}) {
-                Text("Delete it".uppercase())
-            }
-        },
-        dismissButton = {
-            TextButton(onClick = { gameViewModel.updateActiveEdit(false) }) {
-                Text("Cancel".uppercase())
-            }
-        },
-    )
 }
 
 @Composable
@@ -340,6 +330,43 @@ fun mapIntent(uri: Uri) {
     intent.setPackage("com.google.android.apps.maps")
 
     activityContext.startActivity(intent)
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditDialog() {
+    var selectedCuisine = gameViewModel.selectedSquare.name
+
+    Dialog(onDismissRequest = { gameViewModel.updateActiveEdit(false)}) {
+        Surface(
+            shape = RoundedCornerShape(16.dp),
+            color = colorResource(id = R.color.grey_300)
+        ) {
+            Box(modifier = Modifier
+                .size(height = 200.dp, width = 300.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Column(modifier = Modifier.padding(20.dp)) {
+                    Row(
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        TextField(
+                            value = selectedCuisine,
+                            onValueChange = { selectedCuisine = it },
+                            maxLines = 1,
+                            textStyle = TextStyle(color = Color.Black, fontSize = 22.sp, fontWeight = FontWeight.Bold),
+                            colors = TextFieldDefaults.outlinedTextFieldColors(
+                                containerColor = colorResource(id = R.color.grey_50),
+                            )
+                        )
+                    }
+
+                }
+            }
+        }
+
+    }
 }
 
 @Composable
