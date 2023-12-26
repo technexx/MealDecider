96d07b1bda13ba658f4eb4830bd028d0da6e53ae
Brian Salomone <brian.j.salomone@gmail.com>
2023-12-26 14:18:45 -0800

    moved dialog composables to sep class

---
+++ app/src/main/java/meal/decider/DialogComposables.kt
@@ -0,0 +1,196 @@
+package meal.decider
+
+import android.content.Context
+import android.widget.Toast
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Surface
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+
+class DialogComposables(private val activityContext: Context, private val appViewModel: AppViewModel) {
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun AddDialogBox() {
+        var txtField by remember { mutableStateOf("") }
+        val displayedList = appViewModel.displayedCuisineList.collectAsStateWithLifecycle()
+        var searchTerms : List<String>
+
+        //Search box will begin with full list shown.
+        appViewModel.updateDisplayedCuisineList(fullCuisineList)
+
+        Dialog(onDismissRequest = {
+            appViewModel.updateAddMode(false)
+        })
+        {
+            Surface(
+                shape = RoundedCornerShape(16.dp),
+                color = colorResource(id = R.color.grey_300)
+            ) {
+                Box(modifier = Modifier
+                    .size(height = 400.dp, width = 300.dp),
+                ) {
+                    Column(modifier = Modifier
+                        .fillMaxSize()
+                        .padding(20.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.SpaceEvenly)
+                    {
+                        Spacer(modifier = Modifier.height(10.dp))
+                        TextField(
+                            keyboardOptions = KeyboardOptions(capitalization = KeyboardCapitalization.Sentences),
+                            value = txtField,
+                            onValueChange = {
+                                txtField = it
+                                searchTerms = appViewModel.filterList(fullCuisineList, txtField)
+                                appViewModel.updateDisplayedCuisineList(searchTerms)},
+                            singleLine = true,
+                            textStyle = TextStyle(color = Color.Black, fontSize = 22.sp, fontWeight = FontWeight.Bold),
+                            colors = TextFieldDefaults.outlinedTextFieldColors(
+                                containerColor = colorResource(id = R.color.grey_50),
+                            ),
+                        )
+
+                        Spacer(modifier = Modifier.height(10.dp))
+
+                        FullCuisineList(displayedList)
+
+                        Row (modifier = Modifier
+                            .fillMaxWidth(),
+                            horizontalArrangement = Arrangement.SpaceBetween,
+                        ) {
+                            IconButton(onClick = {
+                                appViewModel.updateAddMode(false)
+                            }) {
+                                Icon(
+                                    imageVector = Icons.Filled.Close,
+                                    contentDescription = "",
+                                    tint = colorResource(android.R.color.holo_red_light),
+                                    modifier = Modifier
+                                        .width(50.dp)
+                                        .height(50.dp)
+                                )
+                            }
+                            IconButton(onClick = {
+                                if (!appViewModel.doesCuisineExistsOnBoard(txtField, appViewModel.squareNamesList())) {
+                                    appViewModel.addSquareToList(txtField)
+                                    appViewModel.updateAddMode(false)
+                                } else {
+                                    Toast.makeText(activityContext, "Cuisine already exists!", Toast.LENGTH_SHORT).show()
+                                }
+                            }) {
+                                DialogIcon(imageVector = Icons.Filled.Check, colorResource = android.R.color.holo_green_light)
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun EditDialogBox() {
+        var txtField by remember { mutableStateOf("") }
+
+        txtField = appViewModel.getSquareList[appViewModel.getSquareToEdit].name
+
+        Dialog(onDismissRequest = {
+            appViewModel.updateActiveEdit(false)
+        })
+        {
+            Surface(
+                shape = RoundedCornerShape(16.dp),
+                color = colorResource(id = R.color.grey_300)
+            ) {
+                Box(modifier = Modifier
+                    .size(height = 200.dp, width = 300.dp),
+                ) {
+                    Column(modifier = Modifier
+                        .fillMaxSize()
+                        .padding(20.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.SpaceEvenly)
+                    {
+                        Spacer(modifier = Modifier.height(10.dp))
+                        TextField(
+                            keyboardOptions = KeyboardOptions(capitalization = KeyboardCapitalization.Sentences),
+                            value = txtField,
+                            onValueChange = { txtField = it },
+                            singleLine = true,
+                            textStyle = TextStyle(color = Color.Black, fontSize = 22.sp, fontWeight = FontWeight.Bold),
+                            colors = TextFieldDefaults.outlinedTextFieldColors(
+                                containerColor = colorResource(id = R.color.grey_50),
+                            ),
+                        )
+                        Spacer(modifier = Modifier.weight(1f))
+                        Row (modifier = Modifier
+                            .fillMaxWidth(),
+                            horizontalArrangement = Arrangement.SpaceBetween,
+                        ) {
+                            IconButton(onClick = {
+                                appViewModel.updateActiveEdit(false)
+                            }) {
+                                DialogIcon(imageVector = Icons.Filled.Close, colorResource = android.R.color.holo_red_light)
+                            }
+                            IconButton(onClick = {
+                                appViewModel.updateSquareName(appViewModel.getSquareToEdit, txtField)
+                                appViewModel.updateActiveEdit(false)
+                            }) {
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+
+    @Composable
+    fun DialogIcon(imageVector: ImageVector, colorResource: Int) {
+        Icon(
+            imageVector = imageVector,
+            contentDescription = "",
+            tint = colorResource(colorResource),
+            modifier = Modifier
+                .width(50.dp)
+                .height(50.dp)
+        )
+    }
+}
--- app/src/main/java/meal/decider/MainActivity.kt
+++ app/src/main/java/meal/decider/MainActivity.kt
@@ -1,13 +1,18 @@
 package meal.decider
 
-
-
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -17,21 +22,42 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.selection.selectable
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -39,47 +65,6 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import meal.decider.ui.theme.MealDeciderTheme
-import android.annotation.SuppressLint
-import android.content.Context
-import android.content.Intent
-import android.net.Uri
-import android.widget.AutoCompleteTextView.OnDismissListener
-import android.widget.Toast
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Check
-import androidx.compose.material.icons.filled.Close
-import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.rememberTopAppBarState
-import androidx.compose.runtime.State
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.runtime.snapshots.SnapshotStateList
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.input.KeyboardCapitalization
-import androidx.compose.ui.window.Dialog
 
 //TODO: Categories (Vegan, etc.)
 //TODO: Regions/continents
@@ -88,6 +73,7 @@ import androidx.compose.ui.window.Dialog
 private lateinit var appViewModel : AppViewModel
 @SuppressLint("StaticFieldLeak")
 private lateinit var activityContext : Context
+private lateinit var dialogComposables : DialogComposables
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -99,9 +85,10 @@ class MainActivity : ComponentActivity() {
         appViewModel.createSquareList()
         appViewModel.updateSelectedSquare(appViewModel.getSquareList[0])
 
+        dialogComposables = DialogComposables(activityContext, appViewModel)
+
         setContent {
             MealDeciderTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
@@ -247,11 +234,11 @@ fun SelectionGridLayout() {
     }
 
     if (addMode.value) {
-        AddDialogBox()
+        dialogComposables.AddDialogBox()
     }
 
     if (activeEdit.value) {
-        EditDialogBox()
+        dialogComposables.EditDialogBox()
     }
 
     LazyVerticalGrid(
@@ -395,156 +382,6 @@ fun CuisineListUi(list: List<String>, index: Int, text: String) {
             color = Color.Black,
             text = text )
     }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun AddDialogBox() {
-    var txtField by remember { mutableStateOf("") }
-    val displayedList = appViewModel.displayedCuisineList.collectAsStateWithLifecycle()
-    var searchTerms : List<String>
-
-    //Search box will begin with full list shown.
-    appViewModel.updateDisplayedCuisineList(fullCuisineList)
-
-    Dialog(onDismissRequest = {
-        appViewModel.updateAddMode(false)
-    })
-    {
-        Surface(
-            shape = RoundedCornerShape(16.dp),
-            color = colorResource(id = R.color.grey_300)
-        ) {
-            Box(modifier = Modifier
-                .size(height = 400.dp, width = 300.dp),
-            ) {
-                Column(modifier = Modifier
-                    .fillMaxSize()
-                    .padding(20.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.SpaceEvenly)
-                {
-                    Spacer(modifier = Modifier.height(10.dp))
-                    TextField(
-                        keyboardOptions = KeyboardOptions(capitalization = KeyboardCapitalization.Sentences),
-                        value = txtField,
-                        onValueChange = {
-                            txtField = it
-                            searchTerms = appViewModel.filterList(fullCuisineList, txtField)
-                            appViewModel.updateDisplayedCuisineList(searchTerms)},
-                        singleLine = true,
-                        textStyle = TextStyle(color = Color.Black, fontSize = 22.sp, fontWeight = FontWeight.Bold),
-                        colors = TextFieldDefaults.outlinedTextFieldColors(
-                            containerColor = colorResource(id = R.color.grey_50),
-                        ),
-                    )
-
-                    Spacer(modifier = Modifier.height(10.dp))
-
-                    FullCuisineList(displayedList)
-
-                    Row (modifier = Modifier
-                        .fillMaxWidth(),
-                        horizontalArrangement = Arrangement.SpaceBetween,
-                    ) {
-                        IconButton(onClick = {
-                            appViewModel.updateAddMode(false)
-                        }) {
-                            Icon(
-                                imageVector = Icons.Filled.Close,
-                                contentDescription = "",
-                                tint = colorResource(android.R.color.holo_red_light),
-                                modifier = Modifier
-                                    .width(50.dp)
-                                    .height(50.dp)
-                            )
-                        }
-                        IconButton(onClick = {
-                            if (!appViewModel.doesCuisineExistsOnBoard(txtField, appViewModel.squareNamesList())) {
-                                appViewModel.addSquareToList(txtField)
-                                appViewModel.updateAddMode(false)
-                            } else {
-                                Toast.makeText(activityContext, "Cuisine already exists!", Toast.LENGTH_SHORT).show()
-                            }
-                        }) {
-                            DialogIcon(imageVector = Icons.Filled.Check, colorResource = android.R.color.holo_green_light)
-                        }
-                    }
-                }
-            }
-        }
-
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun EditDialogBox() {
-    var txtField by remember { mutableStateOf("") }
-
-    txtField = appViewModel.getSquareList[appViewModel.getSquareToEdit].name
-
-    Dialog(onDismissRequest = {
-        appViewModel.updateActiveEdit(false)
-    })
-    {
-        Surface(
-            shape = RoundedCornerShape(16.dp),
-            color = colorResource(id = R.color.grey_300)
-        ) {
-            Box(modifier = Modifier
-                .size(height = 200.dp, width = 300.dp),
-            ) {
-                Column(modifier = Modifier
-                    .fillMaxSize()
-                    .padding(20.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.SpaceEvenly)
-                {
-                    Spacer(modifier = Modifier.height(10.dp))
-                    TextField(
-                        keyboardOptions = KeyboardOptions(capitalization = KeyboardCapitalization.Sentences),
-                        value = txtField,
-                        onValueChange = { txtField = it },
-                        singleLine = true,
-                        textStyle = TextStyle(color = Color.Black, fontSize = 22.sp, fontWeight = FontWeight.Bold),
-                        colors = TextFieldDefaults.outlinedTextFieldColors(
-                            containerColor = colorResource(id = R.color.grey_50),
-                        ),
-                    )
-                    Spacer(modifier = Modifier.weight(1f))
-                    Row (modifier = Modifier
-                        .fillMaxWidth(),
-                        horizontalArrangement = Arrangement.SpaceBetween,
-                    ) {
-                        IconButton(onClick = {
-                            appViewModel.updateActiveEdit(false)
-                        }) {
-                            DialogIcon(imageVector = Icons.Filled.Close, colorResource = android.R.color.holo_red_light)
-                        }
-                        IconButton(onClick = {
-                            appViewModel.updateSquareName(appViewModel.getSquareToEdit, txtField)
-                            appViewModel.updateActiveEdit(false)
-                        }) {
-                        }
-                    }
-                }
-            }
-        }
-
-    }
-}
-
-@Composable
-fun DialogIcon(imageVector: ImageVector, colorResource: Int) {
-    Icon(
-        imageVector = imageVector,
-        contentDescription = "",
-        tint = colorResource(colorResource),
-        modifier = Modifier
-            .width(50.dp)
-            .height(50.dp)
-    )
 }
 
 fun mapIntent(uri: Uri) {
