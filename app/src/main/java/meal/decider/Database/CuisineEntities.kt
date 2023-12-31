d1896c922e4d6898639403a4f3cad32ff47f2471
Brian Salomone <brian.j.salomone@gmail.com>
2023-12-31 13:14:39 -0800

    db fixes

--- app/src/main/java/meal/decider/Database/CuisineEntities.kt
+++ app/src/main/java/meal/decider/Database/CuisineEntities.kt
@@ -4,12 +4,10 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity
-class Cuisines {
-    @Entity
-    data class Cuisine(
-        @PrimaryKey val uid: Int,
-        @ColumnInfo(name = "cuisine_name") val name: String?,
-        @ColumnInfo(name = "cuisine_color") val color: Int?
-    )
-}
+//Tablename is what "SELECT * FROM <xxx> refers to".
+@Entity (tableName = "cuisine")
+data class Cuisines(
+    @PrimaryKey val uid: Int,
+    @ColumnInfo(name = "cuisine_name") val name: String?,
+    @ColumnInfo(name = "cuisine_color") val color: Int?
+)
