dca81f4e5d546851dc987ed0e1ea71338b972dc4
Brian Salomone <brian.j.salomone@gmail.com>
2023-12-30 19:56:36 -0800

    fix

--- app/src/main/java/meal/decider/Database/CuisineDao.kt
+++ app/src/main/java/meal/decider/Database/CuisineDao.kt
@@ -5,25 +5,21 @@ import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 
-class CuisineDao {
-    @Dao
-    interface UserDao {
-        @Query("SELECT * FROM cuisine")
-        fun getAll(): List<Cuisines>
+@Dao
+interface CuisineDao {
+    @Query("SELECT * FROM cuisine")
+    fun getAll(): List<Cuisines>
 
-        @Query("SELECT * FROM cuisine WHERE uid IN (:userIds)")
-        fun loadAllByIds(userIds: IntArray): List<Cuisines>
+    @Query("SELECT * FROM cuisine WHERE uid IN (:userIds)")
+    fun loadAllByIds(userIds: IntArray): List<Cuisines>
 
-        @Query("SELECT * FROM cuisine WHERE cuisine_name LIKE :name AND " +
-                "cuisine_color LIKE :color LIMIT 1")
-        fun findByName(name: String, color: Int): Cuisines
+    @Query("SELECT * FROM cuisine WHERE cuisine_name LIKE :name AND " +
+            "cuisine_color LIKE :color LIMIT 1")
+    fun findByName(name: String, color: Int): Cuisines
 
-        @Insert
-        fun insertAll(vararg cuisine: Cuisines)
+    @Insert
+    fun insertAll(vararg cuisine: Cuisines)
 
-        @Delete
-        fun delete(cuisine: Cuisines)
-}
-
-
+    @Delete
+    fun delete(cuisine: Cuisines)
 }
--- app/src/main/java/meal/decider/Database/RoomInteractions.kt
+++ app/src/main/java/meal/decider/Database/RoomInteractions.kt
@@ -2,4 +2,7 @@ package meal.decider.Database
 
 class RoomInteractions (cuisineDatabase: CuisineDatabase.AppDatabase) {
 
+    private val cuisineDao = cuisineDatabase.cuisineDao()
+    val cuisines: List<Cuisines> = cuisineDao.getAll()
+
 }
