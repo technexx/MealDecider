d503fc53fffe5c3b564bbe279bf3d22966070b83
Brian Salomone <brian.j.salomone@gmail.com>
2023-12-31 13:03:53 -0800

    cuisine refs in room now reference SquareValues

--- app/src/main/java/meal/decider/Database/CuisineDao.kt
+++ app/src/main/java/meal/decider/Database/CuisineDao.kt
@@ -4,25 +4,26 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import meal.decider.SquareValues
 
 @Dao
 interface CuisineDao {
     @Query("SELECT * FROM cuisine")
-    fun getAllCuisines(): List<Cuisines>
+    fun getAllCuisines(): List<SquareValues>
 
     @Query("SELECT * FROM cuisine WHERE uid IN (:userIds)")
-    fun getAllCuisinesById(userIds: IntArray): List<Cuisines>
+    fun getAllCuisinesById(userIds: IntArray): List<SquareValues>
 
     @Query("SELECT * FROM cuisine WHERE cuisine_name LIKE :name LIMIT 1")
-    fun getCuisineByName(name: String): Cuisines
+    fun getCuisineByName(name: String): SquareValues
 
     @Query("SELECT * FROM cuisine WHERE cuisine_name LIKE :name AND " +
             "cuisine_color LIKE :color LIMIT 1")
-    fun getCuisineByNameAndColor(name: String, color: Int): Cuisines
+    fun getCuisineByNameAndColor(name: String, color: Int): SquareValues
 
     @Insert
-    fun insertCuisine(vararg cuisine: Cuisines)
+    fun insertCuisine(vararg cuisine: SquareValues)
 
     @Delete
-    fun deleteCuisine(cuisine: Cuisines)
+    fun deleteCuisine(cuisine: SquareValues)
 }
