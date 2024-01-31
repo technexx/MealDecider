ea30e0ca6c849995181c9aaf828c8a0aa8b9ec9d
Brian Salomone <brian.j.salomone@gmail.com>
2024-01-30 16:03:50 -0800

    db file update

--- app/src/main/java/meal/decider/Database/CuisineDatabase.kt
+++ app/src/main/java/meal/decider/Database/CuisineDatabase.kt
@@ -4,9 +4,10 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 
 class CuisineDatabase {
-    @Database(entities = [Cuisines::class], version = 1)
+    @Database(entities = [Cuisines::class, RestaurantFilters::class], version = 1)
     abstract class AppDatabase : RoomDatabase() {
         abstract fun cuisineDao(): CuisineDao
+        abstract fun RestaurantFiltersDao(): RestaurantFiltersDao
     }
 }
 
