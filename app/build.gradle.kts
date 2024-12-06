ea38cb53944fe42db2254829bda5aeaa8a38b072
Brian Salomone <brian.j.salomone@gmail.com>
2024-12-06 15:11:20 -0800

    icon upload and change

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "meal.decider"
         minSdk = 24
         targetSdk = 34
-        versionCode = 4
+        versionCode = 6
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
